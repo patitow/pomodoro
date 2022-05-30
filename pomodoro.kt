63799c4acbd5aaaa9b2ff106cdcc440f47934cce
joaogabrieltg <joaogabrieltg@gmail.com>
2022-05-30 22:32:58 +0000

    Lista parcialmente completa, timer não foi mudado mas agora é a classe pomodoro.kt

--- pomodoro.kt
+++ pomodoro.kt
@@ -52,7 +52,22 @@ fun ciclo(seg:Int, duracao:Int, minuto:Int=duracao-1, t0:Boolean=true){
     }
 }
 
+fun remove(l1:List<String>, indexof:Int):List<String>{
+    return l1.dropLast(l1.size-indexof)+l1.drop(indexof+1)
+}
+fun add(l1:List<String>, entrada:String):List<String>{
+    return listOf(entrada).plus(l1)
+}
+
 fun main() {
+    val l1 = listOf("a1","a2","a3","a4","a5")
+    val l2 = remove(l1,2)
+    val l3 = add(l2,"luciano huck")
+    val l4 = remove(l3,l3.indexOf("luciano huck"))
+    println(l1)
+    println(l2)
+    println(l3)
+    println(l4)
     val s0 = LocalTime.now().second
     ciclo(s0,25)
 }
