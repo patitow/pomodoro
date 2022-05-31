b30383fef71df97ed7d1cf125b16411c6e8a5e08
Patitow <msoliver2002@gmail.com>
2022-05-31 05:04:00 +0000

    Kotlin Coroutines to be imp (Contador too fast)

--- scripts/main.js
+++ scripts/main.js
@@ -5,22 +5,56 @@ var main = function (_, Kotlin) {
   'use strict';
   var println = Kotlin.kotlin.io.println_s8jyv4$;
   var throwCCE = Kotlin.throwCCE;
+  var pause;
   function btnClick() {
     println('Hello, World!');
     contador(1500);
   }
   function contador(t) {
-    var tmp$;
-    var welcomeArea = Kotlin.isType(tmp$ = document.querySelector('div.contador'), HTMLDivElement) ? tmp$ : throwCCE();
-    var str = ((t - t % 60 | 0) / 60 | 0).toString() + ':' + t % 60;
-    welcomeArea.textContent = str;
+    var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3;
+    var divcontador = Kotlin.isType(tmp$ = document.querySelector('div.contador'), HTMLDivElement) ? tmp$ : throwCCE();
+    if (t >= 0 && !pause) {
+      divcontador.textContent = '';
+      if (((t - t % 60 | 0) / 60 | 0) < 10) {
+        var min = '0' + ((t - t % 60 | 0) / 60 | 0);
+        var texto = Kotlin.isType(tmp$_0 = document.createElement('p'), HTMLParagraphElement) ? tmp$_0 : throwCCE();
+        texto.textContent = min;
+        divcontador.append(texto);
+      } else {
+        var min_0 = ((t - t % 60 | 0) / 60 | 0).toString();
+        var texto_0 = Kotlin.isType(tmp$_1 = document.createElement('p'), HTMLParagraphElement) ? tmp$_1 : throwCCE();
+        texto_0.textContent = min_0;
+        divcontador.append(texto_0);
+      }
+      if (t % 60 < 10) {
+        var sec = '0' + t % 60;
+        var texto_1 = Kotlin.isType(tmp$_2 = document.createElement('p'), HTMLParagraphElement) ? tmp$_2 : throwCCE();
+        texto_1.textContent = ':' + sec;
+        divcontador.append(texto_1);
+      } else {
+        var sec_0 = (t % 60).toString();
+        var texto_2 = Kotlin.isType(tmp$_3 = document.createElement('p'), HTMLParagraphElement) ? tmp$_3 : throwCCE();
+        texto_2.textContent = ':' + sec_0;
+        divcontador.append(texto_2);
+      }
+      contador(t - 1 | 0);
+    }
   }
   function main() {
     println('Hello, World!');
   }
+  Object.defineProperty(_, 'pause', {
+    get: function () {
+      return pause;
+    },
+    set: function (value) {
+      pause = value;
+    }
+  });
   _.btnClick = btnClick;
   _.contador_za3lpa$ = contador;
   _.main = main;
+  pause = false;
   main();
   Kotlin.defineModule('main', _);
   return _;
--- scripts/main.kt
+++ scripts/main.kt
@@ -1,5 +1,8 @@
 import kotlinx.browser.*
+import kotlinx.coroutines.*
 import org.w3c.dom.*
+
+var pause: Boolean = false
 
 @JsName("btnClick")
 fun btnClick() {
@@ -7,14 +10,49 @@ fun btnClick() {
 	contador(1500)
 }
 
+
+
 fun contador(t:Int){
-	val welcomeArea = document.querySelector("div.contador") as HTMLDivElement
-	val str = "${(t-t%60)/60}:${t%60}"
-	welcomeArea.textContent="${str}"
+	val divcontador = document.querySelector("div.contador") as HTMLDivElement
+
+	if(t<0||pause){
+		
+	} else{
+		divcontador.textContent=""
+		if(((t-t%60)/60)<10){
+			val min = "0${(t-t%60)/60}"
+			val texto = document.createElement("p") as HTMLParagraphElement
+			texto.textContent = "${min}" 
+			divcontador.append(texto)
+
+		} else{
+			val min = "${(t-t%60)/60}"
+			val texto = document.createElement("p") as HTMLParagraphElement
+			texto.textContent = "${min}" 
+			divcontador.append(texto)
+		}
+		if(t%60<10){
+			val sec = "0${t%60}"
+			val texto = document.createElement("p") as HTMLParagraphElement
+			texto.textContent = ":${sec}" 
+			divcontador.append(texto)
+		} else{
+			val sec = "${t%60}"
+			val texto = document.createElement("p") as HTMLParagraphElement
+			texto.textContent = ":${sec}" 
+			divcontador.append(texto)
+		}
+		
+		contador(t-1)
+	}}
+
+fun main() = runBlocking { // this: CoroutineScope
+    launch { doWorld() }
+    println("Hello")
 }
 
-
-
-fun main() {
-	println("Hello, World!")
+// this is your first suspending function
+suspend fun doWorld() {
+    delay(1000L)
+    println("World!")
 }
