fd325502b0e9d645626cee3ca05be50a748d38d4
joaogabrieltg <joaogabrieltg@gmail.com>
2022-06-03 19:59:21 +0000

    Ciclos feitos

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -8,6 +8,11 @@ var main = function (_, Kotlin) {
   var Unit = Kotlin.kotlin.Unit;
   var tempo;
   var controle;
+  var focos;
+  var shorts;
+  var longs;
+  var totshorts;
+  var totlongs;
   function btnClick() {
     println(controle);
     if (controle === 0) {
@@ -48,35 +53,61 @@ var main = function (_, Kotlin) {
     };
   }
   function contador(t) {
-    var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3;
+    var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5, tmp$_6;
     var divcontador = Kotlin.isType(tmp$ = document.querySelector('div.contador'), HTMLDivElement) ? tmp$ : throwCCE();
+    var divlongs = Kotlin.isType(tmp$_0 = document.getElementById('longbreaks'), HTMLDivElement) ? tmp$_0 : throwCCE();
+    var divshorts = Kotlin.isType(tmp$_1 = document.getElementById('shortbreaks'), HTMLDivElement) ? tmp$_1 : throwCCE();
+    var divciclo = Kotlin.isType(tmp$_2 = document.getElementById('countciclo'), HTMLDivElement) ? tmp$_2 : throwCCE();
     println(controle);
-    if (t < 0 || controle === 2) {
-      println('Encerrou');
+    if (t < 0) {
+      if (longs === 1) {
+        totlongs = totlongs + 1 | 0;
+        focos = 1;
+        longs = 0;
+        shorts = 0;
+        tempo = 1500;
+      } else if (shorts === 3 && focos === 4) {
+        longs = 1;
+        tempo = 900;
+      } else if (shorts === focos) {
+        totshorts = totshorts + 1 | 0;
+        focos = focos + 1 | 0;
+        tempo = 1500;
+      } else {
+        shorts = shorts + 1 | 0;
+        tempo = 300;
+      }
+      controle = 3;
+      btnClick();
+    } else if (controle === 2) {
+      println('Pausa');
     } else {
       divcontador.textContent = '';
       if (((t - t % 60 | 0) / 60 | 0) < 10) {
         var min = '0' + ((t - t % 60 | 0) / 60 | 0);
-        var texto = Kotlin.isType(tmp$_0 = document.createElement('p'), HTMLParagraphElement) ? tmp$_0 : throwCCE();
+        var texto = Kotlin.isType(tmp$_3 = document.createElement('p'), HTMLParagraphElement) ? tmp$_3 : throwCCE();
         texto.textContent = min;
         divcontador.append(texto);
       } else {
         var min_0 = ((t - t % 60 | 0) / 60 | 0).toString();
-        var texto_0 = Kotlin.isType(tmp$_1 = document.createElement('p'), HTMLParagraphElement) ? tmp$_1 : throwCCE();
+        var texto_0 = Kotlin.isType(tmp$_4 = document.createElement('p'), HTMLParagraphElement) ? tmp$_4 : throwCCE();
         texto_0.textContent = min_0;
         divcontador.append(texto_0);
       }
       if (t % 60 < 10) {
         var sec = '0' + t % 60;
-        var texto_1 = Kotlin.isType(tmp$_2 = document.createElement('p'), HTMLParagraphElement) ? tmp$_2 : throwCCE();
+        var texto_1 = Kotlin.isType(tmp$_5 = document.createElement('p'), HTMLParagraphElement) ? tmp$_5 : throwCCE();
         texto_1.textContent = ':' + sec;
         divcontador.append(texto_1);
       } else {
         var sec_0 = (t % 60).toString();
-        var texto_2 = Kotlin.isType(tmp$_3 = document.createElement('p'), HTMLParagraphElement) ? tmp$_3 : throwCCE();
+        var texto_2 = Kotlin.isType(tmp$_6 = document.createElement('p'), HTMLParagraphElement) ? tmp$_6 : throwCCE();
         texto_2.textContent = ':' + sec_0;
         divcontador.append(texto_2);
       }
+      divlongs.textContent = totlongs.toString();
+      divshorts.textContent = totshorts.toString();
+      divciclo.textContent = '#' + (totlongs + 1 | 0);
       tempo = t;
       window.setTimeout(contador$lambda(t), 1000);
     }
@@ -97,6 +128,46 @@ var main = function (_, Kotlin) {
       controle = value;
     }
   });
+  Object.defineProperty(_, 'focos', {
+    get: function () {
+      return focos;
+    },
+    set: function (value) {
+      focos = value;
+    }
+  });
+  Object.defineProperty(_, 'shorts', {
+    get: function () {
+      return shorts;
+    },
+    set: function (value) {
+      shorts = value;
+    }
+  });
+  Object.defineProperty(_, 'longs', {
+    get: function () {
+      return longs;
+    },
+    set: function (value) {
+      longs = value;
+    }
+  });
+  Object.defineProperty(_, 'totshorts', {
+    get: function () {
+      return totshorts;
+    },
+    set: function (value) {
+      totshorts = value;
+    }
+  });
+  Object.defineProperty(_, 'totlongs', {
+    get: function () {
+      return totlongs;
+    },
+    set: function (value) {
+      totlongs = value;
+    }
+  });
   _.btnClick = btnClick;
   _.rstClick = reset;
   _.changePauseButton = changePauseButton;
@@ -104,6 +175,11 @@ var main = function (_, Kotlin) {
   _.contador_za3lpa$ = contador;
   tempo = 1500;
   controle = 0;
+  focos = 1;
+  shorts = 0;
+  longs = 0;
+  totshorts = 0;
+  totlongs = 0;
   Kotlin.defineModule('main', _);
   return _;
 }(typeof main === 'undefined' ? {} : main, kotlin);
--- project/scripts/main.kt
+++ project/scripts/main.kt
@@ -3,6 +3,11 @@ import org.w3c.dom.*
 
 var tempo: Int = 1500
 var controle:Int = 0
+var focos: Int = 1
+var shorts: Int = 0
+var longs: Int = 0
+var totshorts: Int = 0
+var totlongs: Int = 0
 
 @JsName("btnClick")
 fun btnClick() {
@@ -42,17 +47,45 @@ fun changePlayButton() {
 
 fun contador(t:Int){
 	val divcontador = document.querySelector("div.contador") as HTMLDivElement
+	val divlongs = document.getElementById("longbreaks") as HTMLDivElement
+	val divshorts = document.getElementById("shortbreaks") as HTMLDivElement
+	val divciclo = document.getElementById("countciclo") as HTMLDivElement
 	println(controle)
-	if(t<0|| controle==2){
-		println("Encerrou")
-	} else{
+	if(t<0){
+		if(longs==1){
+			totlongs+=1
+			focos=1
+			longs=0
+			shorts=0
+			tempo=1500
+		}
+		else if(shorts==3 && focos==4){
+			longs=1
+			tempo=900
+		}
+		else if(shorts==focos){
+			totshorts+=1
+			focos+=1
+			tempo=1500
+		}
+		else{
+			shorts+=1
+			tempo=300
+		}		
+		controle=3
+		btnClick()
+	}
+	else if(controle==2){
+		println("Pausa")
+	}
+	else{
 		divcontador.textContent=""
 		if(((t-t%60)/60)<10){
 			val min = "0${(t-t%60)/60}"
 			val texto = document.createElement("p") as HTMLParagraphElement
 			texto.textContent = "${min}" 
 			divcontador.append(texto)
-
+			
 		} else{
 			val min = "${(t-t%60)/60}"
 			val texto = document.createElement("p") as HTMLParagraphElement
@@ -70,6 +103,10 @@ fun contador(t:Int){
 			texto.textContent = ":${sec}" 
 			divcontador.append(texto)
 		}
+		divlongs.textContent="${totlongs}"
+		divshorts.textContent="${totshorts}"
+		divciclo.textContent="#${totlongs+1}"
 		tempo=t
 		window.setTimeout({contador(t-1)}, 1000)
-	}}
+	}
+}
