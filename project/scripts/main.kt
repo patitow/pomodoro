55c2dbf7a0401b71f3900cbeb723bb048998ee21
joaogabrieltg <joaogabrieltg@gmail.com>
2022-06-04 04:17:53 +0000

    Barra Inferior Completa

--- project/scripts/main.kt
+++ project/scripts/main.kt
@@ -8,6 +8,8 @@ var shorts: Int = 0
 var longs: Int = 0
 var totshorts: Int = 0
 var totlongs: Int = 0
+var width = 0.00
+var tipo: String = "foco"
 
 @JsName("btnClick")
 fun btnClick() {
@@ -28,8 +30,16 @@ fun btnClick() {
 
 @JsName("rstClick")
 fun reset(){
-	tempo=1500
+	tipo="foco"
+	width=0.00
+	tempo=15
+	focos=1
+	longs=0
+	shorts=0
 	controle=2
+	val barra = document.getElementById("FocoBar") as HTMLDivElement
+	barra.style.width = "${width}%"
+	barra.style.backgroundColor = "#659b65"
 	val divcontador = document.querySelector("div.contador") as HTMLDivElement
 	divcontador.textContent="25:00"
 	changePlayButton()
@@ -46,6 +56,7 @@ fun changePlayButton() {
 }
 
 fun contador(t:Int){
+	val barra = document.getElementById("FocoBar") as HTMLDivElement
 	val divcontador = document.querySelector("div.contador") as HTMLDivElement
 	val divlongs = document.getElementById("longbreaks") as HTMLDivElement
 	val divshorts = document.getElementById("shortbreaks") as HTMLDivElement
@@ -53,6 +64,8 @@ fun contador(t:Int){
 	println(controle)
 	if(t<0){
 		if(longs==1){
+			tipo = "foco"
+			barra.style.backgroundColor = "#659b65"
 			totlongs+=1
 			focos=1
 			longs=0
@@ -60,19 +73,26 @@ fun contador(t:Int){
 			tempo=1500
 		}
 		else if(shorts==3 && focos==4){
+			tipo = "long"
+			barra.style.backgroundColor = "#ffa8a8"
 			longs=1
 			tempo=900
 		}
 		else if(shorts==focos){
+			tipo = "foco"
+			barra.style.backgroundColor = "#659b65"
 			totshorts+=1
 			focos+=1
 			tempo=1500
 		}
 		else{
+			tipo = "short"
+			barra.style.backgroundColor = "#a1a1ff"
 			shorts+=1
 			tempo=300
 		}		
 		controle=3
+		width = 0.00
 		btnClick()
 	}
 	else if(controle==2){
@@ -85,7 +105,6 @@ fun contador(t:Int){
 			val texto = document.createElement("p") as HTMLParagraphElement
 			texto.textContent = "${min}" 
 			divcontador.append(texto)
-			
 		} else{
 			val min = "${(t-t%60)/60}"
 			val texto = document.createElement("p") as HTMLParagraphElement
@@ -107,8 +126,16 @@ fun contador(t:Int){
 		divshorts.textContent="${totshorts}"
 		divciclo.textContent="#${totlongs+1}"
 		tempo=t
+		if(tipo=="foco"){
+			width = width+(100.00/(1500.00))
+		}
+		else if(tipo=="long"){
+			width = width+(100.00/(900.00))
+		}
+		else if(tipo=="short"){
+			width = width+(100.00/(300.00))
+		}
+		barra.style.width = "${width}%"
 		window.setTimeout({contador(t-1)}, 1000)
 	}
 }
-
-//a
